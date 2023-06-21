dddca9d9fedc567d24447137c18760b24d5bec0a
kelso1020 <126113017+kelso1020@users.noreply.github.com>
2023-06-20 20:16:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const getNotes = require('./index.js');
 const postNotes = require('./notes.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
