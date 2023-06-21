8bb9ccebbefc12faac622757d228e4d6f291e4f6
kelso1020 <kelseyjessica1710@gmail.com>
2023-06-20 20:36:37 -0400

    added host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const getNotes = require('./index.js');
 const postNotes = require('./notes.js');
-
+const host = '0.0.0.0',
 const PORT = 3001;
 
 const app = express();
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 app.use(getNotes)
 app.use(postNotes)
 
-app.listen(PORT, () =>
+app.listen(PORT, host, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
