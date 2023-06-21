aaf5564a6a60710f2ee2a5aae2cf235d488fd98f
kelso1020 <kelseyjessica1710@gmail.com>
2023-06-20 20:50:19 -0400

    removed uneccesary code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const getNotes = require('./index.js');
 const postNotes = require('./notes.js');
-const host = '0.0.0.0';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
