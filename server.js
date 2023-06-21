f9cba86309cfca9383f397508fd36ee12ee53cc9
kelso1020 <kelseyjessica1710@gmail.com>
2023-06-20 20:54:43 -0400

    changed comma to semi-colon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const getNotes = require('./index.js');
 const postNotes = require('./notes.js');
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.use(express.static('public'));
 app.use(getNotes)
 app.use(postNotes)
 
-app.listen(PORT, host, () =>
+app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
